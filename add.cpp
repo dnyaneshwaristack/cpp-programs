7cecd2407b08f7cbf5122a85adb2decd1ed58e2e
Dnyaneshwari <dnyaneshwarichate7733@gmail.com>
2026-08-24 10:23:46 +0530

    Add basic C++ program to sum two numbers

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter the first number a:";
+cin>>a;
+cout<<"Enter the secound number b:";
+cin>>b;
+c=a+b;
+cout<<"The sum of given number is"<<c;
+return 0;
+}
